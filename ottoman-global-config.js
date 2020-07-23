1998635312a1d8ec07e9eb215d447458a2f884b2
gsi-alejandro <alejandro@gsoftinnovation.com>
2020-07-23 18:07:37 -0400

    refactor(config): Set disableScopes flag to true
    Refactor connection string to .env file

--- ottoman-global-config.js
+++ ottoman-global-config.js
@@ -1,5 +1,15 @@
 const ottoman = require('ottoman');
+const dotenv = require('dotenv');
+dotenv.config();
 
 ottoman.globalConfig({
-    collectionKey: 'type'
+    collectionKey: 'type',
+    disableScopes: true
+});
+
+ottoman.connect({
+    bucketName: 'travel-sample',
+    connectionString: 'couchbase://localhost:8091',
+    username: 'Administrator',
+    password: 'password'
 });
